391f82a3778d7e3e154971a1bd065a7f7719af9f
Mihajlo <Mihajlo@DESKTOP-QV9P8RR>
2018-11-25 14:19:42 +0100

    resize works

--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -21,12 +21,17 @@ namespace Forms
         private void InitTable()
         {
             TableLayoutPanel Table = new TableLayoutPanel();
-            Table.Location = new Point(0, 24);
-            Table.Size = new Size(100, 100);
+            Table.Location = new Point(10, 24);
+            //Table.Size = new Size(100, 100);
             Table.AutoSize = true;
             Table.Name = "Desk";
-            Table.ColumnCount = 6;
-            Table.RowCount = 10;
+            for (int i = 0; i < 6; i++)
+                Table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100 /6));
+            for (int i = 0; i < 10; i++)
+               Table.RowStyles.Add(new RowStyle(SizeType.Percent, 100 /10));
+
+            /*Table.ColumnCount = 6;
+            Table.RowCount = 10;*/
 
             Table.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             Table.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.AddRows;
@@ -35,15 +40,16 @@ namespace Forms
             {
                 for (int j = 0; j < 10; j++)
                 {
-                    Table.Controls.Add(new PictureBox(), i, j);
-                    PictureBox p = Table.GetControlFromPosition(i, j) as PictureBox;
-                    
+                   
+                    PictureBox p = new PictureBox();
                     p.ImageLocation = "./test.jpg";
                     p.Dock = DockStyle.Fill;
                     p.SizeMode = PictureBoxSizeMode.StretchImage;
+                    Table.Controls.Add(p, i, j);
                 }
             }
-            Table.Location = new Point(0, 24);
+            // Table.Location = new Point(0, 24);
+            Table.Anchor = AnchorStyles.Top;
             Table.Dock = DockStyle.Fill;
         }
      
